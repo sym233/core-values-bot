f57e343930cf56f18b5a3861237e844ac457926d
sym <849228735@qq.com>
2022-08-22 03:40:14 +1000

    update, fix long query error

--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
-import Telegraf from 'telegraf';
-import { InlineQueryResult } from 'telegraf/typings/telegram-types';
+import { Telegraf } from 'telegraf';
+import { InlineQueryResult } from 'telegraf/typings/core/types/typegram';
 
 import { valuesEncoder, valuesDecoder } from './coreValuesEncoder';
 
@@ -10,17 +10,14 @@ function botStart(botToken: string, agent?: any) {
     },
   });
 
-  bot.start((ctx) => ctx.reply('Welcome'));
-  bot.help((ctx) => ctx.reply('Send me a sticker'));
-  bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-  bot.on('inline_query', (ctx) => {
-    const originalText = ctx.inlineQuery?.query || '';
-    // console.log('got inline query');
-    // console.log(originalText);
-    const result: InlineQueryResult[] = [];
-    if (originalText) {
+  bot.start(ctx => ctx.reply('Welcome'));
+  bot.help(ctx => ctx.reply('Type "@CoreValuesBot [Text To Encode]"'));
+  bot.hears('hi', ctx => ctx.reply('Hey there'));
+  bot.on('inline_query', ctx => {
+    const originalText = ctx.inlineQuery?.query as string ?? '';
+    if (originalText && Buffer.from([originalText]).length < 63) {
       const encoded = valuesEncoder(originalText);
-      result.push({
+      const result: InlineQueryResult = {
         type: 'article',
         id: `${Date.now()}${Math.trunc(Math.random() * 1e6)}`,
         title: encoded,
@@ -28,24 +25,28 @@ function botStart(botToken: string, agent?: any) {
           message_text: encoded,
         },
         reply_markup: {
-          inline_keyboard: [[{
-            text: 'Decode',
-            callback_data: originalText,
-          }]]
-        }
-      });
+          inline_keyboard: [
+            [
+              {
+                text: 'Decode',
+                callback_data: originalText,
+              },
+            ],
+          ],
+        },
+      };
+      ctx.answerInlineQuery([result]);
+    } else {
+      ctx.answerInlineQuery([]);
     }
-    // Using context shortcut
-    ctx.answerInlineQuery(result);
   });
-  bot.on('callback_query', (ctx) => {
-    const originalText = ctx.update.callback_query?.data || '';
+  bot.on('callback_query', ctx => {
+    const originalText = ctx.update.callback_query?.data as string ?? '';
     // console.log('got cb query')
-    // console.log(originalText);
     // Using context shortcut
-    ctx.answerCbQuery(originalText, true);
-  })
-  bot.launch();
+    ctx.answerCbQuery(originalText, { show_alert: true });
+  });
+  bot.launch().then(() => console.log('Started'));
 }
 
 export default botStart;
